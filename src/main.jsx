3aac5ad67e6c35435c589f78e9f4c7e556c140f8
INFANTLUVANGO <luvangoinfant@gmail.com>
2025-03-31 17:21:26 +0530

    adding main file

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import Flight from './Flight.jsx'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <Flight/>
+//   </StrictMode>,
+// )
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Portfolio from './Portfolio.jsx'
+//import Portfolio from './Flight.jsx'
+//import Flight from './Flight.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Portfolio/> 
+  </StrictMode>,
+)
+
+
+       
